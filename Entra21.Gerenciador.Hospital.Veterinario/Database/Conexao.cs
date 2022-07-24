cf2bcb698cf2f87932a6d285edfc9e6eb7c8ed66
Gregory Viegas Zimmer <gregory.viegas.zimmer@hotmail.com>
2022-07-24 15:59:39 -0300

    Início trabalho Foi criado o repositório com as principais pastas e serem utilizadas, para que todos os membros da equipe já tenham acesso as mesmas.

---
+++ Entra21.Gerenciador.Hospital.Veterinario/Database/Conexao.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.SqlClient;
+
+namespace Entra21.Gerenciador.Hospital.Veterinario.Database
+{
+    internal class Conexao
+    {
+        public SqlConnection Conectar()
+        {
+            SqlConnection conexao = new SqlConnection();
+
+            var nomeBanco = "BancoDados";
+            var caminhoCompleto = Directory.GetCurrentDirectory().Substring(0, AppContext.BaseDirectory.LastIndexOf("\\bin"));
+
+            conexao.ConnectionString = $@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={caminhoCompleto}\Database\{nomeBanco}.mdf;Integrated Security=True";
+
+            conexao.Open();
+
+            return conexao;
+        }
+    }
+}
