f00c35b50066351cbb51122bf44e8524864db56b
AlanDominguesFernandes28 <alan786k@gmail.com>
2022-07-25 20:11:51 -0300

    Atualizacao trabalho Raca e Pet

---
+++ Entra21.Gerenciador.Hospital.Veterinario/Models/Raca.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Entra21.Gerenciador.Hospital.Vet.Models
+{
+    internal class Raca
+    {
+        public int Id { get; set; }
+        public string Especie { get; set; }
+        public string Nome { get; set; }
+    }
+}
