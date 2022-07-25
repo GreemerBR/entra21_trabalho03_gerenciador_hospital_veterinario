d5a87fb85c260f2a8844eead6301c8e8334dff7f
João Vitor Altermann Martinelli <76072847+joaomarti755@users.noreply.github.com>
2022-07-25 19:27:57 -0300

    Criação e finalização do IResponsavelService

---
+++ Entra21.Gerenciador.Hospital.Veterinario/Services/IResponsavelService.cs
@@ -0,0 +1,13 @@
+﻿using Entra21.Gerenciador.Hospital.Veterinario.Models;
+
+namespace Entra21.Gerenciador.Hospital.Veterinario.Services
+{
+    internal interface IResponsavelService
+    {
+        List<Responsavel> ObterTodos();
+        Responsavel ObterPorId(int id);
+        void Cadastrar(Responsavel responsavel);
+        void Editar(Responsavel responsavel);
+        void Apagar(int id);
+    }
+}
