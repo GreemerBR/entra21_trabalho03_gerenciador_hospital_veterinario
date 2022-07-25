aa87cf5760fdeaf12a9d8deac8d5ffa873e205f4
João Vitor Altermann Martinelli <76072847+joaomarti755@users.noreply.github.com>
2022-07-25 19:17:07 -0300

    Finalização dos Models Endereco e Responsavel

--- Entra21.Gerenciador.Hospital.Veterinario/Models/Endereco.cs
+++ Entra21.Gerenciador.Hospital.Veterinario/Models/Endereco.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Entra21.Gerenciador.Hospital.Veterinario.Models
+﻿namespace Entra21.Gerenciador.Hospital.Veterinario.Models
 {
     internal class Endereco
     {
+        public int Id { get; set; }
+        public Responsavel Responsavel { get; set; }
+        public int Cep { get; set; }
+        public string Logradouro { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string UnidadeFederativa { get; set; }
     }
 }
--- Entra21.Gerenciador.Hospital.Veterinario/Models/Responsavel.cs
+++ Entra21.Gerenciador.Hospital.Veterinario/Models/Responsavel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Entra21.Gerenciador.Hospital.Veterinario.Models
+﻿namespace Entra21.Gerenciador.Hospital.Veterinario.Models
 {
     internal class Responsavel
     {
