d219cce67aac348bf5a8ab76cf21d25e86fa5f52
Gregory Viegas Zimmer <gregory.viegas.zimmer@hotmail.com>
2022-07-25 19:17:26 -0300

    Finalizado Models Foram iniciados e finalizados os Models referente a tabela de Consulta e Veterinario.

---
+++ Entra21.Gerenciador.Hospital.Veterinario/Models/Consulta.cs
@@ -0,0 +1,13 @@
+﻿namespace Entra21.Gerenciador.Hospital.Veterinario.Models
+{
+    internal class Consulta
+    {
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public DateTime Hora { get; set; }
+        public string Observacao { get; set; }
+
+        public Veterinario Veterinario { get; set; }
+        public Pet Pet { get; set; }
+    }
+}
---
+++ Entra21.Gerenciador.Hospital.Veterinario/Models/Veterinario.cs
@@ -0,0 +1,14 @@
+﻿namespace Entra21.Gerenciador.Hospital.Veterinario.Models
+{
+    internal class Veterinario
+    {
+        public int Id { get; set; }
+        public int Idade { get; set; }
+        public int Telefone { get; set; }
+        public int Cpf { get; set; }
+        public int CrmvNumero { get; set; }
+        public string CrmvEstado { get; set; }
+        public string Especialidade { get; set; }
+        public string Nome { get; set; }
+    }
+}
