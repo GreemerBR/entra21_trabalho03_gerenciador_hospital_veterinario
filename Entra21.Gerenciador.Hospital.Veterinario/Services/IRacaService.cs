8471811d72f846f929f930565d1980d2bd47e7d4
AlanDominguesFernandes28 <alan786k@gmail.com>
2022-07-25 21:34:11 -0300

    Nova

--- Entra21.Gerenciador.Hospital.Veterinario/Services/IRacaService.cs
+++ Entra21.Gerenciador.Hospital.Veterinario/Services/IRacaService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entra21.Gerenciador.Hospital.Vet.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,14 @@ using System.Threading.Tasks;
 
 namespace Entra21.Gerenciador.Hospital.Vet.Services
 {
-    internal class IRacaService
+    internal interface IRacaService
     {
+        List<Raca> ObterTodos();
+        Raca ObterPorId(int id);
+        void Cadastrar(Raca raca);
+        void Editar(Raca raca);
+        void Apagar(int id);
+
+
     }
 }
--- Entra21.Gerenciador.Hospital.Veterinario/Services/RacaService.cs
+++ Entra21.Gerenciador.Hospital.Veterinario/Services/RacaService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Entra21.Gerenciador.Hospital.Vet.Database;
+using Entra21.Gerenciador.Hospital.Vet.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,41 @@ using System.Threading.Tasks;
 
 namespace Entra21.Gerenciador.Hospital.Vet.Services
 {
-    internal class RacaService
+    internal class RacaService : IRacaService
     {
+        public void Cadastrar(Raca raca)
+        {
+            var conexao = new Conexao().Conectar();
+
+            var comando = conexao.CreateCommand();
+
+            comando.CommandText = @"INSERT INTO racas (especie, nome)
+            VALUES (@ESPECIE, @NOME);";
+
+            comando.Parameters.AddWithValue("@ESPECIE", raca.Especie);
+            comando.Parameters.AddWithValue("@NOME", raca.Nome);
+           
+            comando.ExecuteNonQuery();         
+
+            comando.Connection.Close();
+        }
+        public void Editar(Raca raca)
+        {
+            throw new NotImplementedException();
+        }
+        public void Apagar(int id)
+        {
+            var conexao = new Conexao().Conectar();
+        }
+        public List<Raca> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+        public Raca ObterPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
     }
 }
