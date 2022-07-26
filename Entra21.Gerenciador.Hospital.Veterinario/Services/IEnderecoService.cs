da03a09be58c3ae9b30e0c313d8bdb92044420c5
João Vitor Altermann Martinelli <76072847+joaomarti755@users.noreply.github.com>
2022-07-26 18:24:55 -0300

    Criação e Finalização do IEnderecoService

---
+++ Entra21.Gerenciador.Hospital.Veterinario/Services/IEnderecoService.cs
@@ -0,0 +1,13 @@
+﻿using Entra21.Gerenciador.Hospital.Vet.Models;
+
+namespace Entra21.Gerenciador.Hospital.Vet.Services
+{
+    internal interface IEnderecoService
+    {
+        List<Endereco> ObterTodos();
+        Endereco ObterPorId();
+        void Cadastrar(Endereco endereco);
+        void Editar(Endereco endereco);
+        void Apagar(int id);
+    }
+}
