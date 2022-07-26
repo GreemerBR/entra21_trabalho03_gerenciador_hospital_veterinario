e8bd3befdd8a3e462ececc8cfb75f1f080499979
AlanDominguesFernandes28 <alan786k@gmail.com>
2022-07-25 21:59:17 -0300

    Raca Service pronto,verificar se esta 100%

--- Entra21.Gerenciador.Hospital.Veterinario/Services/RacaService.cs
+++ Entra21.Gerenciador.Hospital.Veterinario/Services/RacaService.cs
@@ -2,6 +2,7 @@
 using Entra21.Gerenciador.Hospital.Vet.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,20 +29,86 @@ namespace Entra21.Gerenciador.Hospital.Vet.Services
         }
         public void Editar(Raca raca)
         {
-            throw new NotImplementedException();
+            var conexao = new Conexao().Conectar();
+
+            var comando = conexao.CreateCommand();
+
+            comando.CommandText = @"UPDATE racas SET nome = @NOME, especie = @Especie";
+
+            comando.Parameters.AddWithValue("@ESPECIE", raca.Especie);
+            comando.Parameters.AddWithValue("@NOME", raca.Nome);
+           
+            comando.ExecuteNonQuery();
+
+            comando.Connection.Close();
         }
         public void Apagar(int id)
         {
             var conexao = new Conexao().Conectar();
-        }
-        public List<Raca> ObterTodos()
-        {
-            throw new NotImplementedException();
+
+            var comando = conexao.CreateCommand();
+
+            comando.CommandText = "DELETE FROM racas WHERE id = @ID";
+
+            comando.Parameters.AddWithValue("@ID", id);
+
+            comando.ExecuteNonQuery();
+
+            comando.Connection.Close();
         }
         public Raca ObterPorId(int id)
         {
-            throw new NotImplementedException();
+            var conexao = new Conexao().Conectar();
+            var comando = conexao.CreateCommand();
+            comando.CommandText = "SELECT id,especie, nome FROM raca WHERE id = @ID";
+            comando.Parameters.AddWithValue("@ID", id);
+
+            var dataTable = new DataTable();
+            dataTable.Load(comando.ExecuteReader());
+            if (dataTable.Rows.Count == 0)
+                return null;
+
+            var registro = dataTable.Rows[0];
+            var raca = new Raca();
+            raca.Id = Convert.ToInt32(registro["id"]);
+            raca.Especie = registro["especie"].ToString();
+            raca.Nome = registro["nome"].ToString();
+
+
+            conexao.Close();
+
+            return raca;
         }
+        public List<Raca> ObterTodos()
+        {
+            var conexao = new Conexao().Conectar();
+
+            var comando = conexao.CreateCommand();
+
+            comando.CommandText = "SELECT id,especie, nome FROM raca WHERE id = @ID";
+            comando.Parameters.AddWithValue("@ID", id);
+
+            var tabelaEmMemoria = new DataTable();
+
+            tabelaEmMemoria.Load(comando.ExecuteReader());
+
+            if (tabelaEmMemoria.Rows.Count == 0)
+                return null;
+
+            var registro = tabelaEmMemoria.Rows[0];
+
+            var raca = new Raca();
+
+            raca.Id = Convert.ToInt32(registro["id"]);
+            raca.Especie = registro["especie"].ToString();
+            raca.Nome = registro["nome"].ToString();
+            
+
+            comando.Connection.Close();
+
+            return raca;
+        }
+        
 
 
     }
